25493a3a805a4a68adc9ee369397ad389cae343b
son-coder <vison2999@gmail.com>
2025-12-22 05:58:24 +0700

    update code hosonv

--- person/hosonv.js
+++ person/hosonv.js
@@ -1,10 +1,34 @@
-
+console.log("js đã chạy");
 let employees = JSON.parse(localStorage.getItem("employees")) || [];
-function Search() {
-  const keyword = document.getElementById("seach").value.trim().toLowerCase();
-  const result = employees.filter(user => {
-    user.username.toLowerCase().includes(keyword);
-  }); 
-  // includes: dùng để tìm chuỗi con trong chuỗi cha
-  display(result);
+console.log(employees);
+let Form__hsnv = document.querySelector(".form__hsnv");
+
+let userList = Form__hsnv.querySelector("#list");
+
+let rememberRow = null;
+
+function display(Data = employees) {
+  userList.innerHTML = "";
+  Data.forEach((user, index) => {
+    const tr = document.createElement("tr");
+    tr.dataset.index = index;
+    tr.innerHTML = `
+    <td>${user.manv}</td>
+    <td>${user.username}</td>
+    <td>${user.email}</td>
+    <td>
+      <div id="action">
+        <button id="Fix__btn">Sửa thông tin</button>
+        <button id="Xem">Xem chi tiết</button>
+      </div>
+    </td>
+    `;
+    userList.appendChild(tr);
+    tr.querySelector("#Fix__btn").addEventListener("click", () => {
+      rememberRow = tr;
+    });
+  });
 }
+window.addEventListener("load", () => {
+  display(employees);
+});
